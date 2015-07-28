86a4c27fb8e744c94542a606c52ab95c9afa6b1e
lrolleman <lrollema@ualberta.ca>
2015-07-28 11:25:41 -0600

    generate chan id, structure request

--- src/SPH.c
+++ src/SPH.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 void parse(char* message) {
     int* opcode = message;
     switch ((*opcode)) {
         case CHANNEL_OPEN:
             open_channel(message+4);           
             break;
-        case CHAT_SEND:
+        case CHANNEL_RESPONSE:
+            request_channel(message+4);
             break;
     }
+}
+
+void handle_response(char* message) {
+
 }
 void open_channel(char* message) {
     struct chan_open opendata;
@@ -18,10 +23,14 @@ void open_channel(char* message) {
     for (int i=0; i<opendata->rec_count; i++) {
         opendata.rec_list[i] = (message+4)+(64*i);
     }
+    struct chan_request chan_data;
+    chan_data.open_data = opendata;
+    srand(time());
+    chan_data.chan_id = rand();
     //TODO enter channel into table
     //TODO reply to client with channel id
     //TODO send msg CHANNEL_REQUESTED to each recipient 
-
+    
 }
 
 
--- src/SPH.h
+++ src/SPH.h
@@ -1,11 +1,19 @@
-#ifndef SERVER_PROTOCOL_HANDLER_H
-#define SERVER_PROTOCOL_HANDLER_H
+#ifndef SPH_H
+#define SPH_H
 
 struct chan_open {
+    int op_code = CHANNEL_OPEN;
     int* rec_count;
     char** rec_list;
 };
 
+struct chan_request {
+    int op_code = CHANNEL_REQUESTED;
+    struct chan_open open_data;
+    int chan_id;
+};
+
 void parse(char*);
+void handle_response(char*);
 void open_channel(char*);
 #endif
